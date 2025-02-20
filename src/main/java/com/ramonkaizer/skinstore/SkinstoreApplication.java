98dd158c29a9f730a4bc601f914349f642b688f5
ramonKaizer <ramon_kaiser_marques@hotmail.com>
2025-02-20 09:03:38 -0300

    Created project

---
+++ src/main/java/com/ramonkaizer/skinstore/SkinstoreApplication.java
@@ -0,0 +1,13 @@
+package com.ramonkaizer.skinstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkinstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkinstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ramonkaizer/skinstore/SkinstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramonkaizer.skinstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkinstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
